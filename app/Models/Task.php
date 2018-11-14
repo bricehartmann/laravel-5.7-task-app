2560cdb92bd995da10fd8defb65b96916a46c2ce
bricehartmann <bricejhartmann@gmail.com>
2018-11-13 20:48:17 -0600

    add tasks table and Task model

---
+++ app/Models/Task.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_complete' => 'boolean',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'is_complete',
+    ];
+
+    /**
+     * The relationship to the owning user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
