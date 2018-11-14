cee7b65b44b7dc4da6afef6eb007fcec6a1642ac
bricehartmann <bricejhartmann@gmail.com>
2018-11-13 20:06:07 -0600

    move User model to Models dir, add password mutator on User model, add id to password resets table

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -27,4 +27,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Set and encrypt the password attribute.
+     *
+     * @param $value
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
